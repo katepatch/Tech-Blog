5d5b0f16d2400e19277e90660eaae479591fe8c4
kate patch <kate.epatch@gmail.com>
2022-09-08 17:56:56 -0600

    added post.js model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = reequire('sequelize');
-const seequelize = require('../config/connection');
+const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {
