be9cc5712e3aaa1b7e81308bbfcf46f94e7a3c5c
kate patch <kate.epatch@gmail.com>
2022-09-09 13:50:25 -0600

    added LOGOUT route for user

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -117,5 +117,14 @@ router.post('/login', (req, res) => {
 });
 
 //LOGOUT user
+router.post('/logout', withAuth, (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 //DELETE user
