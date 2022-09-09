c65fb3c62f9118bb3e0cd034e12f81e135ead846
kate patch <kate.epatch@gmail.com>
2022-09-08 18:00:28 -0600

    typo

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = reequire('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
