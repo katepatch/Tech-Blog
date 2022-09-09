b9745c487255fbbbfcc21d3e3c6a741cdfc85efc
kate patch <kate.epatch@gmail.com>
2022-09-09 13:42:14 -0600

    added CREATE route for user

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -71,6 +71,22 @@ router.put('/:id', withAuth, (req, res) => {
 });
 
 //CREATE user
+router.post('/', (req, res) => {
+    User.create({
+        user_name: req.body.user_name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(userData => {
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.user_name = userData.user_name;
+            req.session.loggedIn = true;
+
+            res.json(userData);
+        });
+    });
+});
 
 //LOGIN user
 
