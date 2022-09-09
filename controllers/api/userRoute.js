aa44d690c4e122d0a0c3af6655048721698121ec
kate patch <kate.epatch@gmail.com>
2022-09-09 11:40:39 -0600

    added route for all users

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -1,3 +1,27 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+//GET all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['[password]'] }
+    })
+    .then(userData => res.json(userData))
+    .catch(err => {
+        res.status(500).json(err);
+        console.log(err);
+    });
+});
+
+//GET single user
+
+//UPDATE user
+
+//CREATE user
+
+//LOGIN user
+
+//LOGOUT user
+
+//DELETE user
