9ebf6fbeb6ce832a448a65f3434bd012d48d3f1f
kate patch <kate.epatch@gmail.com>
2022-09-12 16:11:47 -0600

    rendering route for single post

--- controllers/index.js
+++ controllers/index.js
@@ -39,3 +39,40 @@ router.get('/login', (req, res) => {
     }
     res.render('login');
 });
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_text', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['user_name']
+                  }
+            },
+            {
+                model: User,
+                attributes: ['user_name']
+            }
+        ]
+    })
+    .then(postData => {
+        if (!postData) {
+            res.status(404).json({ message: 'No post found' });
+            return;
+        }
+
+        const post = postData.get({ plain: true });
+
+        res.render('single-post', { post, loggedIn: req.session.loggedIn});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
