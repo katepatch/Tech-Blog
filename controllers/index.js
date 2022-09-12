0355800f8df577f936641e354b78649c6612a8a7
kate patch <kate.epatch@gmail.com>
2022-09-12 16:01:23 -0600

    added route for posts to show up on homepage

---
+++ controllers/index.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('..models/');
+const sequelize = require('../config/connection');
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    Post.findAll({
+        attributes: ['id', 'post_text', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['user_name']
+                }
+            },
+            {
+                model: User,
+                attributes: ['user_name']
+            }
+        ]
+    })
+    .then(postData => {
+        const posts = postData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
