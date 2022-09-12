443933e33f3f14662358830b60d4147032039885
kate patch <kate.epatch@gmail.com>
2022-09-12 16:40:10 -0600

    index.js for controller routes

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoute');
+
+router.use('/api', apiRoutes);
+router.use('/home', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
