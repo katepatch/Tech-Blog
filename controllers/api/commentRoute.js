1c69bfdd15ea8455a4cccca583f1730472bba5f0
kate patch <kate.epatch@gmail.com>
2022-09-12 15:42:31 -0600

    GET for comments route

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -3,6 +3,14 @@ const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 //GET comments
+router.get('/', (req, res) => {
+    Comment.findAll({})
+    .then(commentData => res.json(commentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
 
 //CREATE comments
 
