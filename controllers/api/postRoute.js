4e3a6b30dbf4eb8aaace127f0213d9e6420e46f1
kate patch <kate.epatch@gmail.com>
2022-09-12 15:12:49 -0600

    added findAll route for post

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -3,3 +3,29 @@ const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 const sequelize = require('../../config/connection');
 
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'post_text', 'title', 'created_at'],
+        
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['user_name']
+                }
+            },
+            {
+                model: User,
+                attributes: ['user_name']
+            },
+        ]
+    })
+    .then(postData => res.json(postData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
