6b26c8f6a3d3378a2cfa432a9abf9bbcf495be69
kate patch <kate.epatch@gmail.com>
2022-09-12 15:44:48 -0600

    CREATE route for comments

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -13,5 +13,18 @@ router.get('/', (req, res) => {
 });
 
 //CREATE comments
-
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id,
+        })
+        .then(commentData => res.json(commentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    }
+});
 //DELETE comments
