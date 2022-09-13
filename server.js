39d2c18ab22db367abe1918ea50d328304a8db49
kate patch <kate.epatch@gmail.com>
2022-09-13 15:18:00 -0600

    tried adding a partial register to server since i keep getting an error

--- server.js
+++ server.js
@@ -31,9 +31,10 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname, '/views/partials');
 
 app.use(routes);
 
-sequelize.sync({force: false}).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`))
 });
