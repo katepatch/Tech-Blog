3e8a41b5b03247f21f0cf862bbd0cb3ff85e170a
kate patch <kate.epatch@gmail.com>
2022-09-13 00:46:04 -0600

    changed up the server.js file to see if the scope was off

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const express = require('express');
+const mysql = require('mysql2');
+const session = require('express-session');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
 const path = require('path');
 
-const helpers = require('./utils/helpers');
-
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({helpers});
-
-const session = require('express-session');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-// app.listen(process.env.PORT || 3000, function(){
-//     console.log("server listening", this.address().port, app.settings.env);
-// });
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const sess = {
     secret: "super secret secret",
-    cookie: { originalMaxAge: 600000 },
+    cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -29,12 +24,13 @@ const sess = {
 };
 
 app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
