5135d11583ec30c8560164ee032e2314d7aed67f
kate patch <kate.epatch@gmail.com>
2022-09-24 23:37:27 -0600

    chaning up server.js scope with hopes it might help

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const mysql = require('mysql2');
-const session = require('express-session');
-const routes = require('./controllers');
-const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
-
 const path = require('path');
+const express = require('express');
+//const mysql = require('mysql2');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
 const sess = {
     secret: "super secret secret",
     cookie: {},
